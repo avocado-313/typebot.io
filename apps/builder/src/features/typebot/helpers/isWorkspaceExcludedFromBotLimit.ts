e7f26a54a1d2c2873e6f560d1ab78625b7fc407f
yasmenmohammed0 <yasmen.abodabash@twerlo.com>
2026-07-14 10:14:38 +0300

    [TEST] Add Chatbot smar t assignation

--- apps/builder/src/features/typebot/helpers/isWorkspaceExcludedFromBotLimit.ts
+++
@@ -1,5 +0,0 @@
-import { env } from '@typebot.io/env'
-
-export const isWorkspaceExcludedFromBotLimit = (workspaceId: string) =>
-  env.BOT_LIMIT_EXCLUDED_WORKSPACE_IDS?.some((id) => id === workspaceId) ??
-  false
