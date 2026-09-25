698222c33feef1a4705bbccdadee47199d2a5283
yasmenmohammed0 <yasmen.abodabash@twerlo.com>
2026-09-25 21:37:32 +0300

    feat: add inputBlockCardLabelColor constant and update BlockCard components to use it

--- apps/builder/src/features/editor/components/BlocksSideBar.tsx
+++ apps/builder/src/features/editor/components/BlocksSideBar.tsx
@@ -16,7 +16,7 @@ import React, { useState } from 'react'
 import { BlockCard } from './BlockCard'
 import { LockedIcon, UnlockedIcon, ListIcon } from '@/components/icons'
 import { BlockCardOverlay } from './BlockCardOverlay'
-import { headerHeight } from '../constants'
+import { headerHeight, inputBlockCardLabelColor } from '../constants'
 import { useTranslate } from '@tolgee/react'
 import { BubbleBlockType } from '@typebot.io/schemas/features/blocks/bubbles/constants'
 import { InputBlockType } from '@typebot.io/schemas/features/blocks/inputs/constants'
@@ -246,7 +246,7 @@ export const BlocksSideBar = () => {
               }
               label={
                 isDraggingNewListBlock ? (
-                  <Text fontSize="sm">
+                  <Text fontSize="sm" color={inputBlockCardLabelColor}>
                     {t('editor.sidebarBlock.list.label')}
                   </Text>
                 ) : undefined
--- apps/builder/src/features/editor/constants.ts
+++ apps/builder/src/features/editor/constants.ts
@@ -1,1 +1,3 @@
 export const headerHeight = 56
+
+export const inputBlockCardLabelColor = '#FFC857'
